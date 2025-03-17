7dfc4fcf39b2c3069b1d17abfeabd465bb88ba74
Darko Golovic <golovicdarko@gmail.com>
2025-03-17 21:46:35 +0100

    fixed small bugs

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -8,8 +8,8 @@ import Spinner from "../../ui/Spinner";
 function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
 
-  if (!bookings.length) return <Empty resourceName="bookings" />;
   if (isLoading) return <Spinner />;
+  if (!bookings.length) return <Empty resourceName="bookings" />;
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
